7f9c5b14acdbf1e1d779953a0c48e654a11ee9cd
adkm2201 <118100878+adkm2201@users.noreply.github.com>
2025-10-07 22:55:41 +0700

    initialzation

---
+++ Line_Coding/src/main/java/com/mycompany/line_coding/Line_Coding.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.line_coding;
+
+/**
+ *
+ * @author adkm2
+ */
+public class Line_Coding {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Line_Coding/src/main/java/en_coding/sequential/AmplitydeModulation.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package en_coding.sequential;
+
+import model.SignalDisplayData;
+
+/**
+ *
+ * @author adkm2
+ */
+public class AmplitydeModulation implements SequentialSignalEncoder{
+
+    @Override
+    public boolean[] textToBin(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getType() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public SignalDisplayData displayData(boolean[] bin) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
